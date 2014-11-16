0dded1509b2ace108ac59036e72f6e55ddc8c709
Ted Jenkins <edwardtjenkins@gmail.com>
2014-11-16 10:07:12 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var io = require('socket.io').listen(4000);
+
+io.sockets.on('connection', function(socket) {
+  socket.on('join', function(data) {
+    io.sockets.emit('userJoined', data);
+    socket.username = data.username;
+  });
+
+  socket.on('ping', function(data) {
+    io.sockets.emit('ping', {username: socket.username});
+  });
+});
