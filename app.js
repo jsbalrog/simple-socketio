e2ecfa9977dd14943981edb59868461b0444da44
jsbalrog <edwardtjenkins@gmail.com>
2014-11-20 15:50:59 -0700

    Added a disconnect event

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var io = require('socket.io').listen(4000);
 
 io.sockets.on('connection', function(socket) {
-  // Listen for join events. When a join event occurs, send
-  // a 'userJoined' event to all clients, and include the
+  // Listen for client-side join events. When a join event occurs, send
+  // a broadcast 'userJoined' event to all other clients, and include the
   // data that was received with the join event.
   socket.on('join', function(data) {
-    io.sockets.emit('userJoined', data);
+    socket.broadcast.emit('userJoined', data);
     socket.username = data.username;
   });
 
-  // Listen for ping events. When a ping event occurs, send
-  // a 'ping' event, and include a username object.
+  // Listen for client-side ping events. When a ping event occurs, send
+  // a broadcast 'ping' event to all other clients, and include a 
+	// username object.
   socket.on('ping', function(data, done) {
-    console.log(done);
-    io.sockets.emit('ping', {username: socket.username});
+    socket.broadcast.emit('ping', {username: socket.username});
     done('ack');
   });
+	
+	socket.on('disconnect', function() {
+		socket.broadcast.emit('userDisconnect', { username: socket.username });
+	});
 });
